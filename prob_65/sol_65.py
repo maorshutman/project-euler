7a10dd50caade1ceb3790aee41c05d92f09e709b
maorshutman <maorus12@gmail.com>
2021-06-18 11:55:02 +0300

    Remove tabs

--- prob_65/sol_65.py
+++ prob_65/sol_65.py
@@ -1,33 +1,32 @@
 
 def main():
+    e_rep = [1, 2]
+    for k in range(2, 35):
+        e_rep += [1, 1, 2 * k]
 
-	e_rep = [1, 2]
-	for k in range(2, 35):
-		e_rep += [1, 1, 2 * k]
+    start_idx = 98
 
-	start_idx = 98
+    numer = e_rep[start_idx]
+    denom = 1
 
-	numer = e_rep[start_idx]
-	denom = 1
+    for i in range(start_idx-1, -1, -1):
+        tmp_num = numer
+        numer = numer * e_rep[i] + denom
+        denom = tmp_num
 
-	for i in range(start_idx-1, -1, -1):
-		tmp_num = numer
-		numer = numer * e_rep[i] + denom
-		denom = tmp_num
+    # Final 1 / (num/denom).
+    denom, numer = numer, denom
+    final_numer = 2 * denom + numer
+    print(final_numer)
 
-	# Final 1 / (num/denom).
-	denom, numer = numer, denom
-	final_numer = 2 * denom + numer
-	print(final_numer)
+    # Sum digits.
+    sum_of_digits = 0
+    while final_numer != 0:
+        sum_of_digits += (final_numer % 10)
+        final_numer = final_numer // 10
 
-	# Sum digits.
-	sum_of_digits = 0
-	while final_numer != 0:
-		sum_of_digits += (final_numer % 10)
-		final_numer = final_numer // 10
-
-	print(f"Sum of digits = {sum_of_digits}")
+    print(f"Sum of digits = {sum_of_digits}")
 
 
 if __name__ == "__main__":
-	main()
+    main()
