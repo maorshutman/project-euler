46cb218b8f13da38fcb75614edaf5da94e44bee9
maorshutman <maorus12@gmail.com>
2021-06-28 22:50:51 +0300

    Remove tabs

--- prob_479/sol_479.py
+++ prob_479/sol_479.py
@@ -17,38 +17,38 @@ def mod_geometric(n, b, m):
 
 def compute_s(n, m=1000000007):
 
-	s = 0
+    s = 0
 
-	for k in range(1, n + 1):
-		print(k)
+    for k in range(1, n + 1):
+        print(k)
 
-		q = 1 - k**2
+        q = 1 - k**2
 
-		s += (mod_geometric(n, q, m) * (q % m)) % m
+        s += (mod_geometric(n, q, m) * (q % m)) % m
 
-	return s % m
+    return s % m
 
 
 def main():
-	""" We use the following realtions between the roots of a cubic equation
-	and its coefficients:
-	
-	a * x^3 + b * x^2 + c * x+ d = 0 
-	
-	q + r + p = -b/a
-	qrp = -d/a
-	qr + pr + qp = c/a
+    """ We use the following realtions between the roots of a cubic equation
+    and its coefficients:
+    
+    a * x^3 + b * x^2 + c * x+ d = 0 
+    
+    q + r + p = -b/a
+    qrp = -d/a
+    qr + pr + qp = c/a
 
-	Then:
+    Then:
 
-	(q + r) * (p + r) * (p + q) = -rpq + (p + r + q) * (c/a) 
-								= d/a - (cb)/(a^2) 
-								= 1 - k^2
+    (q + r) * (p + r) * (p + q) = -rpq + (p + r + q) * (c/a) 
+                                = d/a - (cb)/(a^2) 
+                                = 1 - k^2
 
-	"""
+    """
 
-	print(compute_s(1000000))
+    print(compute_s(1000000))
 
 
 if __name__ == "__main__":
-	main()
+    main()
